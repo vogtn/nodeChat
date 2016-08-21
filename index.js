42e8537a4ccd1fafb522a11766003270b9187b6f
vogtn <vogtn@uw.edu>
2016-08-21 15:05:11 -0700

    changing issues

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ app.get("/", function(req,res){
   res.render("page");
 });
 
+
 app.use(express.static(__dirname+ '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
-io.sockets.on('connection', function(socket){
-  socket.emit('message', {message: 'welcome to the chat'});
-  socket.on('send', function(data){
-    //forwards data to all other sockets
-    io.sockets.emit('message',data);
-  });
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
 });
 
 console.log("Listening on port " + port);
