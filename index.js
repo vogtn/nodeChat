49566c259e7d3068be707a0d7fdc49c8130bc7e0
vogtn <vogtn@uw.edu>
2016-08-21 13:21:29 -0700

    updating index  to handle jade with express

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
-app.get("/", function(req,res){
+/*app.get("/", function(req,res){
   res.send("It works!");
+});
+*/
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req,res){
+  res.render("page");
 });
 
 app.listen(port);
